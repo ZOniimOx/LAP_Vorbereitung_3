f816d60041ca72640c2a608a5a6682ec2b9ea531
Sebastian Schleicher <sebastian.schleicher@flex.com>
2023-03-16 17:47:53 +0100

    added database inserts

--- sql/Database.sql
+++ sql/Database.sql
@@ -16,10 +16,38 @@ create table pc (
     ssdcapacity varchar(10) not null
 );
 
-insert into pc (modelname, cpu, ramcapacity, microphone, webcam, price, ssdcapacity)
+insert into pcs (modelname, cpu, ramcapacity, microphone, webcam, price, ssdcapacity)
 values ('Home Basis', 'i3', '8 GB', true, true, 499, '500 GB'),
 ('Home Pro', 'i5', '8 GB', true, true, 599, '500 GB'),
 ('Home Chef', 'i7', '16 GB', true, true, 799, '500 GB')
 ;
 
-select * from pc;
+insert into additionalparts (partname, price) values ('RTX 2060 Grafikkarte', 89), ('1 TB SSD Erweiterung', 99);
+
+insert into resellers (name, username, password) values ('Media Makrt', 'mmarkt', 'MediaMarkt'), ('Electronic4you', 'e4you', 'Electronic4U');
+
+update orders set resellerid = 1 where orderid = 1;
+
+select * from pcs;
+
+select * from orders;
+
+select * from additionalparts;
+
+
+select * from resellers;
+
+select * from users;
+
+insert into users (username, password, resellerid) values ('mmakrt', 'MediaMakrt!', 1), ('e4you', 'Electronic4U!', 2);
+
+insert into pcorders(pcid, orderid, quantity) values (1, 1, 10);
+
+
+select * from pcorders;
+
+delete from pcorders where pcorderid = 2;
+
+select * from pcorderadditionalparts;
+
+insert into pcorderadditionalparts (pcorderid, partid) values (1, 2);
