55107c00cd8e9ec3ba59ea6506e70c8bdc9c8c8b
Sebastian Schleicher <sebastian.schleicher@flex.com>
2023-05-08 16:46:10 +0200

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { router } from "./main.routers";
+// import * as cors from "cors";
+// var cors = require("cors");
+import cors from "cors";
+// import helmet from "helmet";
 
 class App {
   public app: express.Application;
@@ -10,9 +14,36 @@ class App {
   }
 
   private baseconfig(): void {
+    // this.app.use((req, res, next) => {
+    //   // Website you wish to allow to connect
+    //   res.setHeader("Access-Control-Allow-Origin", "*");
+
+    //   // Request methods you wish to allow
+    //   res.setHeader(
+    //     "Access-Control-Allow-Methods",
+    //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    //   );
+
+    //   // Request headers you wish to allow
+    //   res.header(
+    //     'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept'
+    //   );
+
+    //   // Try to prevent IE compat mode
+    //   res.header("X-UA-Compatible", "IE=edge,chrome=1");
+
+    //   // Pass to next layer of middleware
+    //   next();
+    // });
+    // this.app.use(helmet());
+
+    // Allow connection from local frontend
+
+    this.app.use(cors({ origin: ["http://localhost:4200"] }));
     //Support application/json
     this.app.use(express.json());
     this.app.use("/api", router);
+    // THIS:app.use
   }
 }
 
