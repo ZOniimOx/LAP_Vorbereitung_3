1c3936c7dc81853f2e97d7fbc22caa3761bee07b
Sebastian Schleicher <sebastian.schleicher@flex.com>
2023-05-30 14:53:42 +0200

    allow connection from frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,50 +1,50 @@
-import express from "express";
-import { router } from "./main.routers";
-// import * as cors from "cors";
-// var cors = require("cors");
-import cors from "cors";
-// import helmet from "helmet";
+import express from 'express';
+import { router } from './main.routers';
+import cors from 'cors';
+import helmet from 'helmet';
 
 class App {
-  public app: express.Application;
+	public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.baseconfig();
-  }
+	constructor() {
+		this.app = express();
+		this.baseconfig();
+	}
 
-  private baseconfig(): void {
-    // this.app.use((req, res, next) => {
-    //   // Website you wish to allow to connect
-    //   res.setHeader("Access-Control-Allow-Origin", "*");
+	private baseconfig(): void {
+		// Automatically set various security headers
+		this.app.use(helmet());
+		this.app.use((req, res, next) => {
+			// Website you wish to allow to connect
+			res.setHeader('Access-Control-Allow-Origin', '*');
 
-    //   // Request methods you wish to allow
-    //   res.setHeader(
-    //     "Access-Control-Allow-Methods",
-    //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    //   );
+			// Request methods you wish to allow
+			res.setHeader(
+				'Access-Control-Allow-Methods',
+				'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+			);
 
-    //   // Request headers you wish to allow
-    //   res.header(
-    //     'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept'
-    //   );
+			// Request headers you wish to allow
+			res.header(
+				'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept'
+			);
 
-    //   // Try to prevent IE compat mode
-    //   res.header("X-UA-Compatible", "IE=edge,chrome=1");
+			// Try to prevent IE compat mode
+			res.header('X-UA-Compatible', 'IE=edge,chrome=1');
 
-    //   // Pass to next layer of middleware
-    //   next();
-    // });
-    // this.app.use(helmet());
+			// Pass to next layer of middleware
+			next();
+		});
 
-    // Allow connection from local frontend
+		// Allow connection from local frontend
+		this.app.use(cors());
 
-    this.app.use(cors({ origin: ["http://localhost:4200"] }));
-    //Support application/json
-    this.app.use(express.json());
-    this.app.use("/api", router);
-    // THIS:app.use
-  }
+		//Support application/json
+		this.app.use(express.json());
+
+		// serve router endpoints
+		this.app.use('/api', router);
+	}
 }
 
 export const app = new App().app;
