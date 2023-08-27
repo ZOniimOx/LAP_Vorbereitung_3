364a3484747003538ad4c6a5a3a25e070282b2e4
Sebastian Schleicher <sebastian.schleicher@flex.com>
2023-08-27 12:42:22 +0200

    updated imports order

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { GridModule } from '@progress/kendo-angular-grid';
-import { NavigationModule } from '@progress/kendo-angular-navigation';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/login/login.component';
 import { OrderOverviewComponent } from './components/order-overview/order-overview.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
 import { IconsModule } from '@progress/kendo-angular-icons';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { LayoutModule } from '@progress/kendo-angular-layout';
